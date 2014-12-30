bc1a629f38af854e3b03895dbea2744b2005db72
Charles Meyer <charles.meyer@tufts.edu>
2014-12-30 14:52:53 -0500

    first commit

---
+++ quickstart.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+
+import httplib2
+
+from apiclient.discovery import build
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.file import Storage
+from oauth2client.tools import run
+from oauth2client import tools
+
+def removeNonAscii(s):
+    return "".join([x if ord(x) < 128 else '' for x in s])
+
+def get_messages(label):
+    i = 0
+    if messages['messages']:
+        for m in messages['messages']:
+            print "getting message " + str(i)
+            id = m['id']
+            i+= 1
+            message = gmail_service.users().messages().get(userId='me', id=id).execute()
+            print 'snippet: %s' % removeNonAscii(message['snippet'])
+
+# Path to the client_secret.json file downloaded from the Developer Console
+CLIENT_SECRET_FILE = 'client_secret_388038159163-1lsfdmjo6d2gscar541kqce2595p73aj.apps.googleusercontent.com.json'
+
+# Check https://developers.google.com/gmail/api/auth/scopes for all available scopes
+OAUTH_SCOPE = 'https://www.googleapis.com/auth/gmail.readonly'
+
+# Location of the credentials storage file
+STORAGE = Storage('gmail.storage')
+
+# Start the OAuth flow to retrieve credentials
+flow = flow_from_clientsecrets(CLIENT_SECRET_FILE, scope=OAUTH_SCOPE)
+http = httplib2.Http()
+flags = tools.argparser.parse_args(args=[])
+
+# Try to retrieve credentials from storage or run the flow to generate them
+credentials = STORAGE.get()
+if credentials is None or credentials.invalid:
+    credentials = tools.run_flow(flow, STORAGE, flags, http=http)
+
+# Authorize the httplib2.Http object with our credentials
+http = credentials.authorize(http)
+
+# Build the Gmail service from discovery
+gmail_service = build('gmail', 'v1', http=http)
+
+# Retrieve a page of threads
+messages = gmail_service.users().messages().list(userId='me').execute()
+labels_response = gmail_service.users().labels().list(userId='me').execute()
+labels = labels_response['labels']
+
+for l in labels:
+    print "id " + l['id']
+    print "name " + l['name']
+    get_messages(l['id'])
+    print "-------------------------- ayyyyyyyy lmao--------------------------------"
+    # print "total messages " + str(l['messagesUnread'])
+
+# Print ID for each thread
